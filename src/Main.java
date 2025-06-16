1385024b5b5b53e1986b1dc88835fd72c3a9d6ee
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-16 15:04:55 +0900

    Calculator mul

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+interface Calculator {
+    int compute(int a, int b);
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator mul = new Calculator() {
+            @Override
+            public int compute(int a, int b) {
+                return a * b;
+            }
+        };
+
+        System.out.println(mul.compute(3, 4));
+    }
+}
